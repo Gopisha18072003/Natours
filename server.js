6c08028aaf27afebafca1b1fff723f81ceeaa797
Gopikumarshaw <96327070+Gopisha18072003@users.noreply.github.com>
2024-08-07 15:16:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ mongoose.connect(DB, {
 // });
 
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 7000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 
